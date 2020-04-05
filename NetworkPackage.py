65fb70bb2b369e93f81ba592e182f77445187360
Ron Ittyipe <ronitty1@gmail.com>
2020-04-05 13:53:17 -0600

    Add __str__ to NetworkPackage

--- NetworkPackage.py
+++ NetworkPackage.py
@@ -6,4 +6,11 @@ class NetworkPackage():
         self.laneData = laneData
         self.objects = objects
 
-    
+    def __str__(self):
+        print("----------------")
+        print("Lane LHS: ", self.laneData.lhs)
+        print("Lane POB: ", self.laneData.pob)
+        if (len(self.objects) > 0):
+            print("Obj1 x: ", self.objects[0].x)
+            print("Obj1 y: ", self.objects[0].y)
+        print("----------------")
