ff68177acab1d921358386dae74f01ba9eb184d1
Ron Ittyipe <ronkittyipe@gmail.com>
2020-04-07 17:02:54 -0600

    Add width to ObjData

--- ObjData.py
+++ ObjData.py
@@ -1,11 +1,13 @@
 class ObjData():
     x = 0
     y = 0
+    width = 0
     depth = 0
 
-    def __init__(self, x, y, depth):
+    def __init__(self, x, y, radius, depth):
         self.x = x
         self.y = y
+        self.width = radius * 2
         self.depth = depth
 
     
