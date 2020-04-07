13910b50c7e6447a3abafe0c6430c225cbe741ae
Ron Ittyipe <ronkittyipe@gmail.com>
2020-04-07 16:33:11 -0600

    Update LaneData

--- LaneData.py
+++ LaneData.py
@@ -1,9 +1,13 @@
 class LaneData():
     lhs = 0
     pob = 0
+    lhs_pixel = 0
+    pob_pixel = 0
 
-    def __init__(self, lhs, pob):
+    def __init__(self, lhs, pob, lhs_pixel, pob_pixel):
         self.lhs = lhs
         self.pob = pob
+        self.lhs_pixel = lhs_pixel
+        self.pob_pixel = pob_pixel
 
     
