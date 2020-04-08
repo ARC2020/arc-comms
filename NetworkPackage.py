d0c9b14bbfdb41657dc52338665f453c8a8f34af
Ron Ittyipe <ronkittyipe@gmail.com>
2020-04-08 11:21:09 -0600

    Update print

--- NetworkPackage.py
+++ NetworkPackage.py
@@ -10,6 +10,8 @@ class NetworkPackage():
         print("----------------")
         print("Lane LHS: ", self.laneData.lhs)
         print("Lane POB: ", self.laneData.pob)
+        print("Lane LHS_pixel: ", self.laneData.lhs)
+        print("Lane POB_pixel: ", self.laneData.pob)
         if (len(self.objects) > 0):
             print("Obj1 x: ", self.objects[0].x)
             print("Obj1 y: ", self.objects[0].y)
